5bb23fd303febf330c03a7b4c92982e7e46e48d4
School47 <School47@gmail.com>
2023-06-26 00:14:03 +0500

    4

--- Home4.java
+++ Home4.java
@@ -3,44 +3,53 @@
 //  Требуется восстановить выражение до верного равенства.
 //   Предложить хотя бы одно решение или сообщить, что его нет.
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Home4 {
     public static void main(String[] args) {
         String equation = "";
-        equation = "2? + ?5 = 121"; // Для пользовательского ввода эту строку надо закомментить.
+        equation = "2? + ?5 = 69"; // Для пользовательского ввода эту строку надо закомментить.
         if (equation == "") {
-            System.out.println("Введите n:");
+            System.out.println("Уравнение в строку вида a + b = с (пробелы крайне важны!),некоторые цифры могут быть знаком '?' :");
             Scanner scanner = new Scanner(System.in);
             equation = scanner.nextLine();
             scanner.close();
         }
-        String[] splitEquation = equation.split(" ");
-        String a = splitEquation[0], b = splitEquation[2], c = splitEquation[4];
-        // разбиваем строку на 2 слогаемых и сумму a+b=c, перебираем
-        boolean found = false;
-        for (int i = 0; i <= 9; i++) {
-            for (int j = 0; j <= 9; j++) {
-                int aNum = Integer.parseInt(a.replace("?", String.valueOf(i)));
-                int bNum = Integer.parseInt(b.replace("?", String.valueOf(j)));
-                int cNum = Integer.parseInt(c);
 
-                if (aNum + bNum == cNum) {
-                    System.out.println(a.replace("?", String.valueOf(i)) + " + " +
-                            b.replace("?", String.valueOf(j)) + " = " + c);
-                    found = true;
-                    break;
-                }
-            }
-            if (found) {
-                break;
-            }
-        }
-
-        if (!found) {
+        List<String> solutions = findSolutions(equation);
+        if (solutions.isEmpty()) {
             System.out.println("No solution found");
+        } else {
+            for (String solution : solutions) {
+                System.out.println(solution);
+            }
+        }
+    }
+
+    public static List<String> findSolutions(String equation) {
+        List<String> solutions = new ArrayList<>();
+
+        int index = equation.indexOf("?");
+        if (index == -1) {
+            String[] splitEquation = equation.split(" ");
+            String aStr = splitEquation[0], bStr = splitEquation[2], cStr = splitEquation[4];
+
+            int a = Integer.parseInt(aStr);
+            int b = Integer.parseInt(bStr);
+            int c = Integer.parseInt(cStr);
+            if (a + b == c) {
+                solutions.add(equation);
+            }
+        } else {
+            for (int i = 0; i <= 9; i++) {
+                String newEquation = equation.substring(0, index) + i + equation.substring(index + 1);
+                solutions.addAll(findSolutions(newEquation));
+            }
         }
 
+        return solutions;
     }
 
 }
